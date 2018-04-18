e294a4b2d2a9440bc42690fa4d5cf682f2379b93
Anthony Hauck <33582525+hummingburd@users.noreply.github.com>
2018-04-18 07:51:00 -0700

    Add files via upload

--- docs/classaec_spacer_1_1aec_spacer.js
+++ docs/classaec_spacer_1_1aec_spacer.js
@@ -1,9 +1,9 @@
 var classaec_spacer_1_1aec_spacer =
 [
     [ "__init__", "classaec_spacer_1_1aec_spacer.html#a7e6ab0667eb88bc1c992a57671fe685f", null ],
-    [ "column", "classaec_spacer_1_1aec_spacer.html#a5e7709e6fa93bea7bb222afc6615a006", null ],
     [ "copy", "classaec_spacer_1_1aec_spacer.html#a2bfc9a72d587940830ec82d0b126ce70", null ],
-    [ "place", "classaec_spacer_1_1aec_spacer.html#a7011405a33c1c4c4b2be6b36d436c677", null ],
-    [ "row", "classaec_spacer_1_1aec_spacer.html#acb29eba113fdb0aa6b7a242f1c4181ec", null ],
-    [ "stack", "classaec_spacer_1_1aec_spacer.html#a4fd137b09e644026404655f1fd95f7e7", null ]
+    [ "place", "classaec_spacer_1_1aec_spacer.html#a90265318ead95404a604723d7dc17575", null ],
+    [ "row", "classaec_spacer_1_1aec_spacer.html#a3dba2e51add1aab8db26241a3ccf3783", null ],
+    [ "stack", "classaec_spacer_1_1aec_spacer.html#a938b940428ff52a319b442451fff86d8", null ],
+    [ "stackToArea", "classaec_spacer_1_1aec_spacer.html#a29aa75122307c3989f5ed0696e790a20", null ]
 ];
